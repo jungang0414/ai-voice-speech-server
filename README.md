47c3e6ce41a9910c6c9fa02ef31ca62b92b30d39
jungang0414 <dayr88520@gmail.com>
2024-08-27 16:58:29 +0800

    update: 安裝firebase-admin 並設定具有特殊權限的環境使其可以與firebase進行溝通； 安裝dotenv 設定環境變數的套件，避免金鑰洩漏； 設定.gitignore條件，避免.env上傳至gitHub； Fetch API 抓取資料，篩選後寫入Firebase Database資料庫中。

--- README.md
+++ README.md
@@ -19,3 +19,17 @@ Node.js網頁應用程式框架，建立可以擴展且靈活的網頁和應用�
 簡單來說就是: 從別的網域存取資料
 
 會需要跨域存取是受到[同源政策](https://developer.mozilla.org/zh-TW/docs/Web/Security/Same-origin_policy)的影響，簡單來說就是當A網域要向B網域存取資料，必須要經過B網域的允許才可以存取資料。
+
+#### firebase-admin SDK
+
+[官網](https://firebase.google.com/docs/admin/setup?hl=zh-tw)
+使用的功能如下
+
+- 以完整管理員權限讀取及寫入即時資料庫資料。
+Database: 當使用者於前端與後端溝通時，將後端抓取的資料寫入資料庫 Firebase Database 中。
+
+#### dotenv
+
+[官網](https://www.npmjs.com/package/dotenv)
+
+協助設定環境變數的npm套件，避免未來上線後將自身密鑰公開出去。
--- server.js
+++ server.js
@@ -1,8 +1,26 @@
+var admin = require("firebase-admin");
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 // cors預設所有跨域的請求都接受
 app.use(cors());
+app.use(express.json());
+
+//firebase-admin 私密金鑰設置
+var serviceAccount;
+try {
+    serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY)
+} catch (error) {
+    console.log("Failed private key: Error: ", error);
+}
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://ai-voice-speech.firebaseio.com"
+})
+
+const db = admin.firestore();
 
 app.get("/", function (req, res) {
     try {
@@ -19,12 +37,42 @@ app.get("/api/v1/AllNews", async (req, res) => {
     const today = new Date().toISOString().replaceAll("-", "/").split("T")[0]
     const url = `https://www.taichung.gov.tw/umbraco/Export/ExportDataApi/GetData?contentId=9962&startDate=${today}&endDate=${today}&rows=500&dateAlias=`
     // const url = "https://jsonplaceholder.typicode.com/posts/1"
+
+    const dbCollection = db.collection('taichungNews');
     fetch(url)
         .then((res) => res.json())
-        .then((data) => res.send(data))
+        .then(async (data) => {
+            const newsData = data.Children;
+
+            for (const item of newsData) {
+                const id = item.Id;
+                const name = item.Name || "沒抓到標題的標題";
+                const url = item.Url;
+                const modifyDate = item.ModifyDate;
+                const cleanedContent = item.Properties[6].value.replace(
+                    /<\/?[^>]+(>|$)/g,
+                    ""
+                );
+
+                const q = await dbCollection.where('id', '==', id).get();
+                //確認有無相同資料，並加入新的資料
+                if (q.empty) {
+                    await dbCollection.add({
+                        id: id,
+                        name: name,
+                        content: cleanedContent,
+                        modifyDate: modifyDate,
+                        url: url
+                    });
+                }
+            }
+            res.json({ message: "資料寫入成功" })
+        })
         .catch((error) => {
             res.status(500).send(error);
+            console.log(error);
         });
 })
+
 
 app.listen(3000, console.log(`running on ${3000}`));
